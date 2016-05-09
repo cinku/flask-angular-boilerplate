e0cc118d41c70c12a5d5ef4cb0993d21e23fb136
cinkowski <cinq2ez@gmail.com>
2016-05-09 19:34:10 +0200

    Add basic controller and template

--- static/app/controllers/homeController.ts
+++ static/app/controllers/homeController.ts
@@ -1,9 +1,11 @@
 namespace main.controllers {            
     export class HomeController {
+        users: main.interfaces.User[];
        
         static $inject = ['$scope'];
         
         constructor(private $scope: ng.IScope){
+            this.users = [ { name: "Donald Kaczor", username: "dkacz" }, { name: "Myszka Miki", username: "mmouse"} ];
         }
     }
 }
