b14033ae8d5169f317d3788671fdfab3ea5e1c2c
cinkowski <cinq2ez@gmail.com>
2016-05-09 19:21:26 +0200

    Add typings and tsconfig.json

---
+++ typings.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./typings/angular-material.d.ts" />
+/// <reference path="./typings/angular-ui-router.d.ts" />
+/// <reference path="./typings/angular.d.ts" />
+/// <reference path="./typings/jquery.d.ts" />
